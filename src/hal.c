c10140d673048bb9aed27239c5b3458417e4d09c
Alexey I. Froloff <raorn@altlinux.org>
2008-01-03 19:23:14 +0300

    Gracefully handle DBus and Hal restarts.
    Close connections and wait for DBus and Hal to appear again.
    However, bad things still may happen if DBus/Hal restarts when
    device is being unmounted.
    
    Signed-off-by: Alexey I. Froloff <raorn@altlinux.org>

--- src/hal.c
+++ src/hal.c
@@ -1,5 +1,5 @@
 /*
- * hal.c - Window Maker Volume Manager, HAL integration
+ * hal.c - Window Maker Volume Manager, Hal integration
  *
  * Copyright (C) 2005 by Sir Raorn <raorn@altlinux.org>
  *
@@ -43,21 +43,14 @@
 
 #define warn(fmt,arg...) g_warning("%s/%d: " fmt,__FILE__,__LINE__,##arg)
 
-static dbus_bool_t hal_mainloop_integration(LibHalContext *ctx, DBusError *error)
-{
-	DBusConnection *dbus_connection;
+static DBusConnection *dbus_connection = NULL;
+static LibHalContext *hal_context = NULL;
 
-	dbus_connection = dbus_bus_get(DBUS_BUS_SYSTEM, error);
-
-	if (dbus_error_is_set(error))
-		return FALSE;
-
-	dbus_connection_setup_with_g_main(dbus_connection, NULL);
-
-	libhal_ctx_set_dbus_connection(ctx, dbus_connection);
-
-	return TRUE;
-}
+static DBusHandlerResult signal_filter(DBusConnection *connection, DBusMessage *message, void *user_data);
+static gboolean init_dbus_connection(void);
+static gboolean init_hal_context(void);
+static void deinit_hal_context(void);
+static gboolean reinit_dbus_connection(gpointer data);
 
 static void hal_device_added(LibHalContext *ctx, const char *udi)
 {
@@ -237,90 +230,222 @@ static void hal_device_condition(LibHalContext *ctx, const char *udi, const char
 	dbg("Device condition: %s, %s, %s\n", udi, condition_name, condition_detail);
 }
 
-
-gboolean wmvm_do_hal_init(void)
+static DBusHandlerResult signal_filter(DBusConnection *connection, DBusMessage *message, void *user_data)
 {
-	LibHalContext *ctx;
+	if (dbus_message_get_type(message) != DBUS_MESSAGE_TYPE_SIGNAL)
+		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+
+	/* Disconnected from DBus */
+	if (dbus_message_is_signal(message, DBUS_INTERFACE_LOCAL, "Disconnected")) {
+		dbg("disconnected from DBus\n");
+		deinit_hal_context();
+		dbus_connection_unref(dbus_connection);
+		dbus_connection = NULL;
+		g_timeout_add(1000, reinit_dbus_connection, NULL);
+		return DBUS_HANDLER_RESULT_HANDLED;
+	}
+
+	/* Check for signal NameOwnerChange */
+	if (dbus_message_is_signal(message, DBUS_INTERFACE_DBUS, "NameOwnerChanged")) {
+		DBusError error;
+		char *service;
+		char *old_owner = NULL;
+		char *new_owner = NULL;
+
+		dbus_error_init(&error);
+		if (dbus_message_get_args(message, &error,
+								  DBUS_TYPE_STRING, &service,
+								  DBUS_TYPE_STRING, &old_owner,
+								  DBUS_TYPE_STRING, &new_owner,
+								  DBUS_TYPE_INVALID)) {
+			if (strcmp(service, "org.freedesktop.Hal") == 0) {
+				dbg("NameOwnerChanged for org.freedesktop.Hal\n");
+				gboolean old_owner_good, new_owner_good;
+
+				old_owner_good = (old_owner != NULL && (strlen(old_owner) > 0));
+				new_owner_good = (new_owner != NULL && (strlen(new_owner) > 0));
+
+				if (!old_owner_good && new_owner_good) {
+					dbg("Hal just appeared\n");
+					init_hal_context();
+					dbus_error_free(&error);
+					return DBUS_HANDLER_RESULT_HANDLED;
+				} else if (old_owner_good && !new_owner_good) {
+					dbg("Hal just disappeared\n");
+					deinit_hal_context();
+					dbus_error_free(&error);
+					return DBUS_HANDLER_RESULT_HANDLED;
+				}
+			}
+		} else {
+			warn("error geting NameOwner of org.freedesktop.Hal: %s\n", error.message);
+			dbus_error_free(&error);
+			return DBUS_HANDLER_RESULT_HANDLED;
+		}
+
+		dbus_error_free(&error);
+	}
+
+	return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
+}
+
+static gboolean init_dbus_connection(void)
+{
 	DBusError error;
-	char **devices;
+
+	dbus_error_init(&error);
+	dbus_connection = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
+
+	if ((dbus_connection == NULL) || dbus_error_is_set(&error)) {
+		warn("can't get DBus connection: %s\n", error.message);
+		dbus_error_free(&error);
+		return FALSE;
+	}
+
+	dbus_connection_setup_with_g_main(dbus_connection, NULL);
+	dbus_connection_set_exit_on_disconnect(dbus_connection, FALSE);
+	dbus_connection_add_filter(dbus_connection, signal_filter, NULL, NULL);
+
+	dbus_bus_add_match(dbus_connection,
+					   "type='signal',"
+					   "interface='" DBUS_INTERFACE_DBUS "',"
+					   "sender='" DBUS_SERVICE_DBUS "',"
+					   "member='NameOwnerChanged'",
+					   &error);
+
+	dbus_error_free(&error);
+	return TRUE;
+}
+
+static gboolean init_hal_context(void)
+{
+	DBusError error;
 	char **volumes;
 	char *udi;
 	int i;
 	int nr;
 
-	if (!(ctx = libhal_ctx_new())) {
-		warn("failed to initialize HAL!\n");
+	if (dbus_connection == NULL)
 		return FALSE;
-	}
+
+	if (hal_context != NULL)
+		return TRUE;
 
 	dbus_error_init(&error);
-	if (!hal_mainloop_integration(ctx, &error)) {
-		warn("hal_initialize failed: %s\n", error.message);
+	if (!dbus_bus_name_has_owner(dbus_connection, "org.freedesktop.Hal", &error) || dbus_error_is_set(&error)) {
+		warn("Hal is not running: %s\n", error.message);
 		dbus_error_free(&error);
 		return FALSE;
 	}
 
-	libhal_ctx_set_device_added(ctx, hal_device_added);
-	libhal_ctx_set_device_removed(ctx, hal_device_removed);
-	libhal_ctx_set_device_new_capability(ctx, hal_device_new_capability);
-	libhal_ctx_set_device_lost_capability(ctx, hal_device_lost_capability);
-	libhal_ctx_set_device_property_modified(ctx, hal_property_modified);
-	libhal_ctx_set_device_condition(ctx, hal_device_condition);
-
-	if (!libhal_device_property_watch_all(ctx, &error)) {
-		warn("failed to watch all HAL properties!: %s\n", error.message);
+	if ((hal_context = libhal_ctx_new()) == NULL) {
+		warn("failed to allocate Hal context\n");
 		dbus_error_free(&error);
-		libhal_ctx_free(ctx);
 		return FALSE;
 	}
 
-	if (!libhal_ctx_init(ctx, &error)) {
-		warn("hal_initialize failed: %s\n", error.message);
+	libhal_ctx_set_dbus_connection(hal_context, dbus_connection);
+
+	libhal_ctx_set_device_added(hal_context, hal_device_added);
+	libhal_ctx_set_device_removed(hal_context, hal_device_removed);
+	libhal_ctx_set_device_new_capability(hal_context, hal_device_new_capability);
+	libhal_ctx_set_device_lost_capability(hal_context, hal_device_lost_capability);
+	libhal_ctx_set_device_property_modified(hal_context, hal_property_modified);
+	libhal_ctx_set_device_condition(hal_context, hal_device_condition);
+
+	if (!libhal_ctx_init(hal_context, &error) || dbus_error_is_set(&error)) {
+		warn("failed to initialize Hal context: %s\n", error.message);
 		dbus_error_free(&error);
-		libhal_ctx_free(ctx);
+		libhal_ctx_free(hal_context);
+		hal_context = NULL;
 		return FALSE;
 	}
 
-
-	/*
-	 * Do something to ping the HAL daemon - the above functions will
-	 * succeed even if hald is not running, so long as DBUS is.  But we
-	 * want to exit silently if hald is not running, to behave on
-	 * pre-2.6 systems.
-	 */
-	devices = libhal_get_all_devices(ctx, &nr, &error);
-	if (!devices) {
-		warn("seems that HAL is not running: %s\n", error.message);
+	if (!libhal_device_property_watch_all(hal_context, &error) || dbus_error_is_set(&error)) {
+		warn("failed to watch all Hal properties: %s\n", error.message);
 		dbus_error_free(&error);
-
-		libhal_ctx_shutdown(ctx, NULL);
-		libhal_ctx_free(ctx);
+		libhal_ctx_shutdown(hal_context, NULL);
+		libhal_ctx_free(hal_context);
+		hal_context = NULL;
 		return FALSE;
 	}
-	libhal_free_string_array(devices);
 
-	volumes = libhal_find_device_by_capability(ctx, "volume", &nr, &error);
+	volumes = libhal_find_device_by_capability(hal_context, "volume", &nr, &error);
 	if (dbus_error_is_set(&error)) {
 		warn("could not find volume devices: %s\n", error.message);
 		dbus_error_free(&error);
-
-		libhal_ctx_shutdown(ctx, NULL);
-		libhal_ctx_free(ctx);
+		libhal_ctx_shutdown(hal_context, NULL);
+		libhal_ctx_free(hal_context);
+		hal_context = NULL;
 		return FALSE;
 	}
 
 	for (i = 0; i < nr; i++) {
 		udi = volumes [i];
 
-		hal_device_added(ctx, udi);
-		if (libhal_device_property_exists(ctx, udi, "volume.is_mounted", NULL) &&
-		    libhal_device_get_property_bool(ctx, udi, "volume.is_mounted", NULL)) {
-			hal_property_modified(ctx, udi, "volume.is_mounted", FALSE, FALSE);
-			hal_property_modified(ctx, udi, "volume.mount_point", FALSE, FALSE);
+		hal_device_added(hal_context, udi);
+		if (libhal_device_property_exists(hal_context, udi, "volume.is_mounted", NULL) &&
+		    libhal_device_get_property_bool(hal_context, udi, "volume.is_mounted", NULL)) {
+			hal_property_modified(hal_context, udi, "volume.is_mounted", FALSE, FALSE);
+			hal_property_modified(hal_context, udi, "volume.mount_point", FALSE, FALSE);
 		}
 	}
 
 	libhal_free_string_array(volumes);
+	dbus_error_free(&error);
+	return TRUE;
+}
+
+static void deinit_hal_context(void)
+{
+	wmvm_remove_all_volumes();
+
+	if (hal_context != NULL) {
+		/* Whenever I try to shutdown Hal context if DBus connection is dropped,
+		 * I get SIGSEGV.  Check DBus connection before shutting down Hal context */
+		if (dbus_connection != NULL && dbus_connection_get_is_connected(dbus_connection))
+			libhal_ctx_shutdown(hal_context, NULL);
+		libhal_ctx_free(hal_context);
+		hal_context = NULL;
+	}
+
+	return;
+}
+
+static gboolean reinit_dbus_connection(gpointer data)
+{
+	dbg("reinit connection\n");
+	if (dbus_connection == NULL) {
+		dbg("... DBus\n");
+		if (init_dbus_connection() != TRUE) {
+			return TRUE;
+		}
+	}
+
+	if (dbus_connection != NULL && hal_context == NULL) {
+		dbg("... Hal\n");
+		if (init_hal_context() != TRUE) {
+			return TRUE;
+		}
+	}
+
+	dbg("connection reinitialized\n");
+	return FALSE;
+}
+
+
+gboolean wmvm_do_hal_init(void)
+{
+	if (!init_dbus_connection()) {
+		return FALSE;
+	}
+
+	if (!init_hal_context()) {
+		dbus_connection_unref(dbus_connection);
+		dbus_connection = NULL;
+		return FALSE;
+	}
+
 	return TRUE;
 }
 
